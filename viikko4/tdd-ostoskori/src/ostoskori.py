880d575e90f8dc58e55971ea97f0637d0687c165
kerkkanen <katjaker@cubbli20-gold.local>
2022-02-09 13:39:49 +0200

    Yhden tuotteen lisäämisen jälkeen korissa yksi tavara toimii

--- viikko4/tdd-ostoskori/src/ostoskori.py
+++ viikko4/tdd-ostoskori/src/ostoskori.py
@@ -3,20 +3,15 @@ from ostos import Ostos
 
 class Ostoskori:
     def __init__(self):
-        # ostoskori tallettaa Ostos-oliota, yhden per korissa oleva Tuote
         self.ostokset = []
 
     def tavaroita_korissa(self):
-        # kertoo korissa olevien tavaroiden lukumäärän
-        # eli jos koriin lisätty 2 kpl tuotetta "maito", tulee metodin palauttaa 2 
-        # samoin jos korissa on 1 kpl tuotetta "maito" ja 1 kpl tuotetta "juusto", tulee metodin palauttaa 2
         sum = 0
         for ostos in self.ostokset:
             sum += ostos.lukumaara()
         return sum
 
     def hinta(self):
-        # kertoo korissa olevien ostosten yhteenlasketun hinnan
         sum = 0
         for ostos in self.ostokset:
             sum += ostos.hinta
@@ -24,8 +19,8 @@ class Ostoskori:
 
 
     def lisaa_tuote(self, lisattava: Tuote):
-        # lisää tuotteen
-        pass
+        ostos = Ostos(lisattava)
+        self.ostokset.append(ostos)
 
     def poista_tuote(self, poistettava: Tuote):
         # poistaa tuotteen
@@ -36,6 +31,4 @@ class Ostoskori:
         # tyhjentää ostoskorin
 
     def ostokset(self):        
-        # palauttaa listan jossa on korissa olevat ostos-oliot
-        # kukin ostos-olio siis kertoo mistä tuotteesta on kyse JA kuinka monta kappaletta kyseistä tuotetta korissa on
         return self.ostokset
