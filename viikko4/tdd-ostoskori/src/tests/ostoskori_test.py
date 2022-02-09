bb2a4127297075d53f659c0bf3787ae313bdb67d
kerkkanen <katjaker@cubbli20-gold.local>
2022-02-09 14:58:17 +0200

    Kahden saman tuotteen lisäämisen jälkeen korin ostoksen nimi tuotteen nimi ja määrä kaksi toimii

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -62,3 +62,10 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(self.maito)
         ostokset = self.kori.ostokset 
         self.assertEqual(len(ostokset), 1)
+
+    def test_kahden_saman_tuotteen_lisaamisen_jalkeen_korissa_ostos_jolla_tuotteen_nimi_ja_maara(self):
+        self.kori.lisaa_tuote(self.maito)
+        self.kori.lisaa_tuote(self.maito)
+        ostokset = self.kori.ostokset 
+        self.assertEqual(ostokset[0].tuotteen_nimi(), "Maito")
+        self.assertEqual(ostokset[0].lukumaara(), 2)
