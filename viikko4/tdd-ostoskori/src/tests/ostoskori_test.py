5637ce2bf139276a4b9b38ce5ea777cdd0e345e3
kerkkanen <katjaker@cubbli20-gold.local>
2022-02-09 13:56:26 +0200

    Kahden saman tuotteen lisäämisen jälkeen korin hinta on tuotteiden hinta toimii

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -34,3 +34,8 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(self.maito)
         self.kori.lisaa_tuote(self.maito)
         self.assertEqual(self.kori.tavaroita_korissa(), 2)
+
+    def test_kahden_tuotteen_lisaamisen_jalkeen_korin_hinta_on_tuotteiden_hinta(self):
+        self.kori.lisaa_tuote(self.maito)
+        self.kori.lisaa_tuote(self.maito)
+        self.assertEqual(self.kori.hinta(), 6)
